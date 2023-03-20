0f3ac96dc5c50b9ff9ad001f7d339c9d884eba3e
osawa-koki <github@osawa.dev>
2023-03-20 21:53:28 +0900

    サイズ調整まで。

--- app/main.rb
+++ app/main.rb
@@ -13,8 +13,21 @@ height = image.rows
 
 puts "width:#{width}, height:#{height}"
 
+# セルの大きさを設定する
+cell_width = 0.7
+cell_height = 5
+
 Axlsx::Package.new do |p|
   p.workbook.add_worksheet(:name => "main") do |sheet|
+
+    (0..height-1).each do |y|
+      sheet.add_row Array.new(width, nil)
+      sheet.rows[y].height = cell_height
+    end
+
+    (0..width-1).each do |x|
+      sheet.column_info[x].width = cell_width
+    end
 
     # ピクセルを縦横ループして1ピクセルずつ色を取得する
     (0..height-1).each do |y|
@@ -28,6 +41,10 @@ Axlsx::Package.new do |p|
 
         # puts "x:#{x}, y:#{y}, color:#{red},#{green},#{blue}"
         # puts "color_code:#{color_code}"
+
+        bg = p.workbook.styles.add_style :bg_color => color_code
+
+        sheet.rows[y].cells[x].style = sheet.workbook.styles.add_style(:bg_color => 'FF0000')
 
       end
     end
