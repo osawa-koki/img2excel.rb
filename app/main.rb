e16365fe93a7742e210c63adcc5c70d5e83ac4e2
osawa-koki <github@osawa.dev>
2023-03-20 20:59:24 +0900

    セルサイズ変更の背景色のセット。

--- app/main.rb
+++ app/main.rb
@@ -1,12 +1,30 @@
 require 'axlsx'
 
 Axlsx::Package.new do |p|
-  p.workbook.add_worksheet(:name => "Pie Chart") do |sheet|
-    sheet.add_row ["Simple Pie Chart"]
-    %w(first second third).each { |label| sheet.add_row [label, rand(24)+1] }
-    sheet.add_chart(Axlsx::Pie3DChart, :start_at => [0,5], :end_at => [10, 20], :title => "example 3: Pie Chart") do |chart|
-      chart.add_series :data => sheet["B2:B4"], :labels => sheet["A2:A4"],  :colors => ['FF0000', '00FF00', '0000FF']
-    end
+  p.workbook.add_worksheet(:name => "main") do |sheet|
+    # セルの値を設定
+    sheet.add_row ["Sample Spreadsheet"]
+    sheet.add_row ["First", "Second", "Third"]
+    sheet.add_row [1, 2, 3]
+
+    # 行の高さを設定
+    sheet.rows[0].height = 30
+    sheet.rows[1].height = 20
+    sheet.rows[2].height = 25
+
+    # 列の幅を設定
+    sheet.column_widths 5, 15, 10
+
+    # スタイルを作成
+    red_bg = p.workbook.styles.add_style :bg_color => "FF0000"
+    green_bg = p.workbook.styles.add_style :bg_color => "00FF00"
+    blue_bg = p.workbook.styles.add_style :bg_color => "0000FF"
+
+    # セルにスタイルを適用
+    sheet["A2"].style = red_bg
+    sheet["B2"].style = green_bg
+    sheet["C2"].style = blue_bg
   end
-  p.serialize('simple.xlsx')
+
+  p.serialize('sample.xlsx')
 end
