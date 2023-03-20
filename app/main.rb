47334e291e5e8081d50780623cac0acc000e6f77
osawa-koki <github@osawa.dev>
2023-03-20 21:37:35 +0900

    loop

--- app/main.rb
+++ app/main.rb
@@ -11,43 +11,27 @@ image = Magick::Image.read(image_path).first
 width = image.columns
 height = image.rows
 
+puts "width:#{width}, height:#{height}"
+
 Axlsx::Package.new do |p|
   p.workbook.add_worksheet(:name => "main") do |sheet|
 
     # ピクセルを縦横ループして1ピクセルずつ色を取得する
     (0..height-1).each do |y|
-
-      sheet.add_row Array.new(width, nil)
-
       (0..width-1).each do |x|
         pixel = image.pixel_color(x, y)
-        red = pixel.red
-        green = pixel.green
-        blue = pixel.blue
+        red = pixel.red / 256
+        green = pixel.green / 256
+        blue = pixel.blue / 256
+
+        color_code = "%02X%02X%02X" % [red, green, blue]
 
         # puts "x:#{x}, y:#{y}, color:#{red},#{green},#{blue}"
-
+        # puts "color_code:#{color_code}"
 
       end
     end
 
-    # # 行の高さを設定
-    # sheet.rows[0].height = 30
-    # sheet.rows[1].height = 20
-    # sheet.rows[2].height = 25
-
-    # # 列の幅を設定
-    # sheet.column_widths 5, 15, 10
-
-    # # スタイルを作成
-    # red_bg = p.workbook.styles.add_style :bg_color => "FF0000"
-    # green_bg = p.workbook.styles.add_style :bg_color => "00FF00"
-    # blue_bg = p.workbook.styles.add_style :bg_color => "0000FF"
-
-    # # セルにスタイルを適用
-    # sheet["A2"].style = red_bg
-    # sheet["B2"].style = green_bg
-    # sheet["C2"].style = blue_bg
   end
 
   p.serialize('sample.xlsx')
